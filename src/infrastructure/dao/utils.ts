2d26ffd03d34d81551dd657348fa21b2b780c24a
lleonori <lorenzo.leonori.93@gmail.com>
2024-12-06 17:52:13 +0100

    Fix linting problem

--- src/infrastructure/dao/utils.ts
+++ src/infrastructure/dao/utils.ts
@@ -7,17 +7,14 @@ export function buildSortBy<
   Table extends Exclude<keyof DB, "schemaversion">,
   Model extends Record<string, unknown>,
   Alias extends string = Table,
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  O extends object = {},
+  O extends object = object,
 >(
   sortBy: SortBy<Model>,
-  alias?: Alias,
+  alias?: Alias
 ): ReadonlyArray<OrderByExpression<DB, Table, O>> {
   return sortBy.map(
     ([field, order]) =>
-      `${alias ? `${alias}.` : ""}${snakeCase(field as string)} ${
-        order ?? "asc"
-      }`,
+      `${alias ? `${alias}.` : ""}${snakeCase(field as string)} ${order ?? "asc"}`
   ) as unknown as ReadonlyArray<OrderByExpression<DB, Table, O>>;
 }
 
