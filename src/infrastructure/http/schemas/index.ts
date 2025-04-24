2b09d9d7d86f0ccf66e69d4e3aefed953c218119
lleonori <lorenzo.leonori.93@gmail.com>
2025-04-24 16:02:53 +0200

    Rename some folders

--- src/infrastructure/http/schemas/index.ts
+++ src/infrastructure/http/schemas/index.ts
@@ -1,6 +1,6 @@
-export { default as BookingSchemas } from "./bookings/index.js";
-export { default as CoachSchemas } from "./coachs/index.js";
-export { default as TimetableSchemas } from "./timetables/index.js";
+export { default as BookingSchemas } from "./booking/index.js";
+export { default as CoachSchemas } from "./coach/index.js";
+export { default as TimetableSchemas } from "./timetable/index.js";
 export { default as CalendarSchemas } from "./calendar/index.js";
 export { default as WeekdaySchemas } from "./weekday/index.js";
 export { default as WeekdayTimeSchemas } from "./weekday-time/index.js";
