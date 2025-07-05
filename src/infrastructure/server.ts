a3255fc31c66b92dba828a8d9fbe69f8e09fce2e
lleonori <lorenzo.leonori.93@gmail.com>
2025-07-05 14:58:23 +0200

    Fix CORS issue

--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -12,7 +12,10 @@ const __dirname = dirname(__filename);
 export default async function (app: FastifyInstance) {
   app.register(import("@fastify/auth"));
   app.register(import("@fastify/cors"), {
-    origin: [/^http:\/\/localhost:\d+$/],
+    origin: [
+      /^http:\/\/localhost:\d+$/,
+      /^https:\/\/neroniana-powerlifting\.pl:3000$/,
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   });
   app.register(import("@fastify/sensible"));
